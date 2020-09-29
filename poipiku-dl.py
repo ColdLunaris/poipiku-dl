5ad396c3fb44f926357bf4a47b462023ca142ce9
Sindre Ødegårdstuen Sletten <sisl0403@fs-innlandet.no>
2020-09-30 00:33:06 +0200

    Revert "Added support for whole users, custom output-folder, quiet mode, custom dir for geckodriver and code-cleanup"
    This reverts commit 2133926c61bec0091f58d7f808890a3fb8698aeb.

--- poipiku-dl.py
+++ poipiku-dl.py
@@ -1,112 +1,60 @@
-﻿import sys, re, os, time, requests, argparse
-
+﻿import sys, re, os, time, requests
 from termcolor import colored
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
-from pathlib import Path
+
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
+# Get URL and current working directory for file-naming
+site = sys.argv[1]
+cwd = os.getcwd()
 
-# Check if defined dir-path exists. If not, throw exception
-def dir_path(string):
-    if os.path.isdir(string):
-        return string
-    else:
-        raise Directory(string)
+# Set firefox to be quiet and load webpage
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(options=options)
+driver.get(site)
 
-# Create argument-parser. Supported arguments are output-dir, quiet and URL
-def create_arg_parser():
-    parser = argparse.ArgumentParser('python3 poipiku-dl.py', 
-                                     description='a tool to download pictures from Poipiku')
-    parser.add_argument('-d', dest='PATH',
-                        help='path to directory where pictures will be saved', type=dir_path)
-    parser.add_argument('-g', dest='geckodriver', default=False,
-                        help='set path to geckodriver executable. If not set, it\'s assumed that executable is in the same directory as script')
-    parser.add_argument('-q', default=False,
-                        help='run without output', action='store_true')
-    parser.add_argument('URL', help='address to account to be downloaded. Also works with only a set of pictures')
-    return parser
+# Check for "Show more" buttons. If present, load all images, else ignore
+button = driver.execute_script("return document.getElementsByClassName('BtnBase IllustItemExpandBtn')[0]")
+if button:
+    driver.execute_script("document.getElementsByClassName('BtnBase IllustItemExpandBtn')[0].click()")
+    time.sleep(1)
+else:
+    pass
 
-# Turn off logging, set driver in headless-mode and specify geckodriver-location if needed
-def setup_webdriver():
-    options = Options()
-    options.headless = True
-    if parsed_args.geckodriver is False:
-        driver = webdriver.Firefox(service_log_path=os.path.devnull, options=options)
-    else:
-        driver = webdriver.Firefox(service_log_path=os.path.devnull, options=options, executable_path=parsed_args.geckodriver)
-    return driver
+# Parse page-source and extract only picture-links
+src = driver.page_source
+soup = BeautifulSoup(src, 'html.parser')
+img_tags = soup.find_all('img', {'src':re.compile('.jpg')})
 
-# Check for "Show more" buttons. If present, show all images, else ignore
-def javascript_button_clicker():
-    count = 0
-    buttons = driver.execute_script("return document.getElementsByClassName('BtnBase IllustItemExpandBtn')")
-    for button in buttons:
-        driver.execute_script("document.getElementsByClassName('BtnBase IllustItemExpandBtn')[" + str(count) + "].click()")
-        count += 1
-        time.sleep(1)
-    else:
-        pass
+for img in img_tags:
+    # Fix URLs and load image
+    path = img['src']
+    url = re.sub(r"//", "https://", path)
+    response = requests.get(url)
 
-# Parse page-source and extract only image-links
-def get_image_links():
-    src = driver.page_source
-    soup = BeautifulSoup(src, 'html.parser')
-    images = soup.find_all('img', {'src':re.compile('.jpg')})
-    return images
-
-# Create filenames. Replace redudant info in filename
-def set_filename(url):
+    # Create a filename. Replace redudant info in filename
     a = urlparse(url)
     filename = os.path.basename(a.path)
     filename = re.sub(r".jpeg_640.jpg", ".jpg", filename)
-    filename = re.sub(r".png_640.jpg", ".jpg", filename)
-    return filename
 
-arg_parser = create_arg_parser()
-parsed_args = arg_parser.parse_args(sys.argv[1:])
-
-cwd = os.getcwd()
-
-# Set firefox to be quiet, load webpage and get all pictures
-driver = setup_webdriver()
-driver.get(parsed_args.URL)
-javascript_button_clicker()
-images = get_image_links()
-
-for image in images:
-    # Fix URLs and load image
-    path = image['src']
-    url = re.sub(r"//", "https://", path)
-
-    # Ignore warning-pictures
-    if "/warning" in path:
-        continue
-
-    response = requests.get(url)
-    filename = set_filename(url)
-
-    os.chdir(parsed_args.PATH)
-    cwd = os.getcwd()
-    
-    # Save images to working dir/specified dir unless they already exist
+    # If file already exists, print green text and don't save anything
     if os.path.isfile(filename):
-        if parsed_args.q is False:
-            if os.name == 'nt':
-                print(colored(cwd + "\\" + filename, 'green'))
-            else:
-                print(colored(cwd + "/" + filename, 'green'))
+        # Find OS-version, and apply folder structure to show complete filepath
+        if os.name == 'nt':
+            print(colored(cwd + "\\" + filename, 'green'))
         else:
-            continue
+            print(colored(cwd + "/" + filename, 'green'))
+        continue
     else:
+        # Write image to file. Print full path in default terminal colors afterwards
         with open(filename, 'wb') as f:
-                f.write(response.content)
-        if parsed_args.q is False:
-            if os.name == 'nt':
-                print(cwd + "\\" + filename)
-            else:
-                print(cwd + "/" + filename)
+            f.write(response.content)
+        if os.name == 'nt':
+            print(cwd + "\\" + filename)
         else:
-            continue
+            print(cwd + "/" + filename)
+
 driver.close()
